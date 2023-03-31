7ef08435293a146087fbe439c1eeb290782b8a59
Henry James <henryjaames@gmail.com>
2023-03-31 14:39:37 +0100

    added otodojo web piece and added a little bit of interactivity to the background spinning shape

--- src/index.js
+++ src/index.js
@@ -2,32 +2,34 @@
 import * as UTIL from './usefulFunctions.js'
 import './main.css'
 
-// // track mouse clicks
-// let mouseX, mouseY = 0
+// track mouse clicks
+let mouseX, mouseY = 0
 
 
-// function logKey(e) {
-//     mouseX = e.clientX - 100
-//     mouseY = e.clientY - 100
-//     if (mouseX < 0) mouseX = 0
-//     if (mouseY < 0) mouseY = 0
-// }
-// window.addEventListener('pointermove', logKey)
+function logKey(e) {
+    mouseX = e.clientX - 100
+    mouseY = e.clientY - 100
+    if (mouseX < 0) mouseX = 0
+    if (mouseY < 0) mouseY = 0
+}
+window.addEventListener('pointerdown', logKey)
 
-// let start, previousTimeStamp
-// const backgroundElement = document.getElementById("mainImage")
-// function step(timestamp) {
-//     if (start === undefined) {
-//         start = timestamp
-//     }
+let start, previousTimeStamp, previousMouseX, previousMouseY
+const backgroundElement = document.getElementById("mainImage")
+function step(timestamp) {
+    if (start === undefined) {
+        start = timestamp
+    }
 
-//     if (timestamp != previousTimeStamp) {
-//         backgroundElement.style.transform = `translate(${mouseX}px,${mouseY}px)`
-//         console.log(mouseX, mouseY)
-//     }
+    if (timestamp != previousTimeStamp) {
+        if (mouseX !== previousMouseX || mouseY !== previousMouseY) {
+            backgroundElement.style.top = `${mouseY}px`;
+            backgroundElement.style.left = `${mouseX}px`;
+            console.log(mouseX, mouseY)
+        }
+    }
+    previousTimeStamp = timestamp
+    window.requestAnimationFrame(step)
+}
 
-//     previousTimeStamp = timestamp
-//     window.requestAnimationFrame(step)
-// }
-
-// window.requestAnimationFrame(step)
+window.requestAnimationFrame(step)
